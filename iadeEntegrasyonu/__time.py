c34458ac0d1110474cbd311eb67fec7d68b784b9
Muhammed Sevinçtekin <50045401+msevnctkn@users.noreply.github.com>
2024-10-17 16:01:31 +0300

    Delete iadeEntegrasyonu directory

--- iadeEntegrasyonu/__time.py
+++
@@ -1,4 +0,0 @@
-import datetime
-
-def ms_to_datetime(ms):
-    return datetime.datetime.fromtimestamp(ms/1000.0)
--- iadeEntegrasyonu/trendyol_sdk/utils.py
+++
@@ -1,4 +0,0 @@
-from json import dumps
-
-def json_encode(data, ensure_ascii=False, encoding="utf-8"):
-    return dumps(data, ensure_ascii=ensure_ascii).encode('utf-8')
